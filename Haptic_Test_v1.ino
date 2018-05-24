a21b543037cda7c46e6bf4bb0eb44a161e8c1c22
Ruben Sponselee <24604490+RubenSpons@users.noreply.github.com>
2018-05-24 12:00:08 +0200

    Update Haptic_Test_v1.ino
    Actually got the right file this time.

--- Haptic_Test_v1.ino
+++ Haptic_Test_v1.ino
@@ -1,9 +1,35 @@
-void setup() {
-  // put your setup code here, to run once:
+// PWM pins on UNO: 3, 5, 6, 9, 10, and 11.
+int pwmPins[] = {3, 5, 6, 9, 10, 11};
+int pinCount = 6;
 
-}
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void setup() { 
+  for (int thisPin = 0; thisPin < pinCount; thisPin++) {
+    pinMode(pwmPins[thisPin], OUTPUT);
+  }
+  Serial.begin(9600);
+  while (!Serial);
+  Serial.println("Insert connected PWM Pin for Haptic Sensor");
+} 
 
-}
+ 
+void loop() { 
+  if (Serial.available()) {    
+    int hapticPin = Serial.parseInt();
+    
+    Serial.println("Running tests on pin: ");
+    Serial.println(hapticPin);
+    Serial.println("\nIncreasing vibration");
+    for (int i=0; i <= 255; i++){
+      analogWrite(hapticPin, i);
+      delay(10);
+    }
+    Serial.println("Decreasing vibration");
+    for (int i=255; i >= 0; i--){
+      analogWrite(hapticPin, i);
+      delay(10);
+    }
+
+   Serial.println("Testing finsihed, feel free to select another PWM Pin. \n \n");
+  }
+} 
